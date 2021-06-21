545c71a04c2a2297b8905017ca86eaa00acf494f
Marit Lebakken <57687030+marit-lebakken@users.noreply.github.com>
2021-06-20 21:53:56 -0500

    Added icons for edit and delete, spacing not completely working.

--- src/component/body/body.js
+++ src/component/body/body.js
@@ -3,6 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -16,14 +22,28 @@ export default function Body(){
   const classes = useStyles();
 
   return (<div>
-    <input type="text" placeholder="Tasks"/>
-    <button class="button">Add task</button>
+
+    <form  noValidate autoComplete="off">
+      <TextField id="filled-basic" label="Enter task" variant="filled" />
+    </form>
+
+    <Button variant="contained" color="primary">
+        Add to List
+    </Button>
+
     <List>
       <ListItem button>
-        <ListItemText primary="List Item #1"/>
-      </ListItem>
-      <ListItem button>
-        <ListItemText primary="List Item #2"/>
+        <ListItemText primary="Sample List Item"/>
+        <ListItemSecondaryAction>
+            <IconButton edge = "start" aria-label="delete">
+                <EditIcon />
+            </IconButton>
+        </ListItemSecondaryAction>
+        <ListItemSecondaryAction>
+            <IconButton edge = "end" aria-label="delete">
+                <DeleteIcon />
+            </IconButton>
+        </ListItemSecondaryAction>
       </ListItem>
     </List>
   </div>)
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -8,11 +8,10 @@ const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
     },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
     title: {
       flexGrow: 1,
+      flexDirection: "column",
+      justifyContent: "center",
     },
   }));
 
